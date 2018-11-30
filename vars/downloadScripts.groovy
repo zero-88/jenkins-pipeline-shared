528243537ffc0ed9d170317cc9eff7f556fb536e
zero-88 <sontt246@gmail.com>
2018-11-30 21:34:26 +0700

    Fix url for docker utils script

--- vars/downloadScripts.groovy
+++ vars/downloadScripts.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String folder = '/tmp') {
-    sh "curl -o ${folder}/docker-join-network.sh https://gist.githubusercontent.com/zero-88/ca37f7adeaeee13fabc7591aad4ba2f3/raw/4d6d2b1d309cef831aa59b759dad7a7973e1eb9d/docker-join-network.sh"
-    sh "curl -o ${folder}/genpwd.sh https://gist.githubusercontent.com/zero-88/ad4b61759f2e260182de5e67b3c47484/raw/42bf913bc60df4eaa1b5e3473f80dc1ceea7ac08/genpwd.sh"
+    sh "curl -o ${folder}/docker-join-network.sh https://raw.githubusercontent.com/zero-88/devops-utils/master/docker/docker-join-network.sh"
+    sh "curl -o ${folder}/genpwd.sh https://raw.githubusercontent.com/zero-88/devops-utils/master/genpwd.sh"
     sh "chmod +x ${folder}/*.sh"
 }
